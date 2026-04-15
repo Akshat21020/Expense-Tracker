afb8824fbe1ebdc454a3dcf5d730e6a06a24a1be
Akshat21020 <gargaksht2102@gmail.com>
2026-04-15 21:58:40 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
